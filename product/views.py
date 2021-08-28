5bd01050067e69c6301790814b58bacc92f41d0c
benily <leekwokhinben@gmail.com>
2021-08-28 16:46:16 +0800

    clean up

--- product/views.py
+++ product/views.py
@@ -1,14 +1,11 @@
 from django.http.response import JsonResponse
-from django.shortcuts import render
-from rest_framework.renderers import JSONRenderer
-from rest_framework.decorators import renderer_classes, api_view
+from rest_framework.decorators import api_view
 
 from product.models import Product
 from product.serializers import ProductSerializer
 
 
 @api_view(['GET'])
-@renderer_classes([JSONRenderer])
 def api_products(request):
     products = Product.objects.all()
     serializers = ProductSerializer(products, many=True)
