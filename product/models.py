3ebc61be049f03565ab499df2bc552dea435020a
benily <leekwokhinben@gmail.com>
2021-08-28 16:13:33 +0800

    change model of product

--- product/models.py
+++ product/models.py
@@ -1,12 +1,10 @@
 from django.db import models
 
-# Create your models here.
-
 
 class Product(models.Model):
-    name = models.CharField(max_length=255)
-    description = models.CharField(max_length=255)
+    name = models.CharField(max_length=255, unique=True)
     cost = models.DecimalField(max_digits=6, decimal_places=1)
+    description = models.TextField()
     date_created = models.DateTimeField(auto_now_add=True)
     date_modified = models.DateTimeField(auto_now=True)
 
